9524b70faa591e4722f56003057909569ece8d1e
Enio <enio.filho@deal.com.br>
2023-07-03 14:27:32 -0300

    adding prisma and a store

--- components/modals/storeModal.tsx
+++ components/modals/storeModal.tsx
@@ -1,11 +1,53 @@
 "use client";
 
-import { FC } from "react";
+import { FC, useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import axios from "axios";
+
 import { Modal } from "@/components/ui/modal";
 import { useStoreModal } from "@/hooks/useStoreModal";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { toast } from "react-hot-toast";
+
+const formSchema = z.object({
+  name: z.string().min(3).max(30),
+});
 
 const StoreModal: FC = ({}) => {
   const { isOpen, onClose } = useStoreModal();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    setIsLoading(true);
+    try {
+      const response = await axios.post("/api/stores", values);
+      toast.success("Store created successfully");
+      onClose();
+    } catch (error) {
+      toast.error("Something went wrong");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
     <Modal
       title="Create store"
@@ -13,7 +55,46 @@ const StoreModal: FC = ({}) => {
       isOpen={isOpen}
       onClose={onClose}
     >
-      Future Create Store Form
+      <div>
+        <div className="space-y-4 py-2 pb-4">
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Name</FormLabel>
+                    <FormControl>
+                      <Input
+                        disabled={isLoading}
+                        placeholder="E-Commerce"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage>
+                      {form.formState.errors.name?.message}
+                    </FormMessage>
+                  </FormItem>
+                )}
+              />
+              <div className="pt-6 space-x-2 flex items-center justify-end w-full">
+                <Button
+                  variant={"outline"}
+                  onClick={onClose}
+                  disabled={isLoading}
+                  type="button"
+                >
+                  Cancel
+                </Button>
+                <Button type="submit" disabled={isLoading}>
+                  Continue
+                </Button>
+              </div>
+            </form>
+          </Form>
+        </div>
+      </div>
     </Modal>
   );
 };
